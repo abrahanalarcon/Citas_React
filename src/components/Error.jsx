afd67f2ca6b269e10988ccbd3fb5067bfd1bbb94
abrahanalarcon <moiseshazim@gmail.com>
2024-01-30 19:50:39 -0400

    Agregando martgin top a la alarma para que se vea mas estatico y no tan pegado de los inputs

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,6 +1,6 @@
 const Error = ({mensaje}) => {
   return (
-    <div className="bg-red-600 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
+    <div className="bg-red-600 text-white text-center p-3 uppercase font-bold mb-3 rounded-md mt-1.5">
       <p>{mensaje}</p>
     </div>
   )
